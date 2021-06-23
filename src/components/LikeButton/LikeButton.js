32df5cea80f791a641a8248c4390200ee2109a09
b_bado <bado.bernard94@gmail.com>
2021-06-23 17:09:37 +0200

    adding button

--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { ReactComponent as Hand } from "./hand.svg";
 
 import "./styles.scss";
 
 const LikeButton = () => {
-  return <button className="like-button-wrapper">Like</button>;
+  return (
+    <button className="like-button-wrapper">
+      <div className="like-button">
+        <Hand />
+        <span>Like</span>
+        <span className="suffix">d</span>
+      </div>
+    </button>
+  );
 };
 
 export default LikeButton;
