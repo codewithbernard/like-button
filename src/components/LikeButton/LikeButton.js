1191585913ae03d6686a44383490be3db23c8abc
b_bado <bado.bernard94@gmail.com>
2021-06-25 18:44:30 +0200

    adding boom animation

--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -4,19 +4,36 @@ import { ReactComponent as Hand } from "./hand.svg";
 
 import "./styles.scss";
 
+const particleList = Array.from(Array(10));
+
 const LikeButton = () => {
   const [liked, setLiked] = useState(false);
   const [clicked, setClicked] = useState(0);
 
   return (
     <button
-      key={clicked}
       onClick={() => {
         setLiked(!liked);
         setClicked(clicked + 1);
       }}
       className={cn("like-button-wrapper", { liked, clicked: clicked > 0 })}
     >
+      {liked && (
+        <div className="particles">
+          {particleList.map((_, index) => (
+            <div
+              className="particle-rotate"
+              style={{
+                transform: `rotate(${
+                  (360 / particleList.length) * index + 1
+                }deg)`,
+              }}
+            >
+              <div className="particle-tick" />
+            </div>
+          ))}
+        </div>
+      )}
       <div className="like-button">
         <Hand />
         <span>Like</span>
